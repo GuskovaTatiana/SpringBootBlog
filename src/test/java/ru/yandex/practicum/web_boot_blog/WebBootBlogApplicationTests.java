4c08b5b4149f0c54f8dd85fc5898422c181b1350
terokhovets <terokhovets@mdaudit.ru>
2025-08-09 16:22:37 +0300

    web-boot-blog. first commit

---
+++ src/test/java/ru/yandex/practicum/web_boot_blog/WebBootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.web_boot_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBootBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
